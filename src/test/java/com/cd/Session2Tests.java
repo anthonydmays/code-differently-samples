542bef89379362c63b9286e819ba9332d9aa0c16
Anthony D. Mays <anthony@morganlatimer.com>
2023-12-20 13:44:29 -0800

    chore: Adding coverage for Session2

---
+++ src/test/java/com/cd/Session2Tests.java
@@ -0,0 +1,10 @@
+package com.cd;
+
+import org.junit.jupiter.api.Test;
+
+public class Session2Tests {
+    @Test
+    public void main_executesSuccessfully() {
+        Session2.main(new String[0]);
+    }
+}
